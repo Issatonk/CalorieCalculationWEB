25898ecd53388783fc61ce1a52e25e82ef4d81db
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-09-23 19:01:22 +0300

    Add entities and dbContext

--- CalorieCalculation.DataAccess.Sqlite/CalorieCalculationContext.cs
+++ CalorieCalculation.DataAccess.Sqlite/CalorieCalculationContext.cs
@@ -1,7 +1,17 @@
-﻿namespace CalorieCalculation.DataAccess.Sqlite
-{
-    public class CalorieCalculationContext
-    {
+﻿using CalorieCalculation.DataAccess.Sqlite.Entities;
+using Microsoft.EntityFrameworkCore;
 
+namespace CalorieCalculation.DataAccess.Sqlite
+{
+    public class CalorieCalculationContext : DbContext
+    {
+        public CalorieCalculationContext(DbContextOptions<CalorieCalculationContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<FoodConsumed> FoodsConsumed { get; set; }
     }
 }
