5673f1069e228b85a8754e31b8b10b679b2b6d05
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-09-24 13:45:19 +0300

    Add ProductRepository

--- CalorieCalculation.DataAccess.Sqlite/Repository/ProductRepository.cs
+++ CalorieCalculation.DataAccess.Sqlite/Repository/ProductRepository.cs
@@ -1,5 +1,7 @@
-﻿using CalorieCalculation.Core;
+﻿using AutoMapper;
+using CalorieCalculation.Core;
 using CalorieCalculation.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +12,49 @@ namespace CalorieCalculation.DataAccess.Sqlite.Repository
 {
     public class ProductRepository : IProductRepository
     {
-        public Task<Product> CreateProduct(Product product)
+        private readonly CalorieCalculationContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(CalorieCalculationContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public Task<bool> DeleteProduct(int id)
+        public async Task<Product> CreateProduct(Product product)
         {
-            throw new NotImplementedException();
+            if(await _context.Products.FirstOrDefaultAsync(x=>x.Name == product.Name) != null)
+            {
+                throw new ArgumentException("product is exists");
+            }
+            var newProduct = _mapper.Map<Core.Product, Entities.Product>(product);
+            var result = await _context.Products.AddAsync(newProduct);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<Entities.Product,Core.Product>(result.Entity);
+        }
+        public async Task<Product> GetProduct(int id)
+        {
+            var result = await _context.Products.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return _mapper.Map<Entities.Product, Core.Product>(result);
+        }
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            var result = await _context.Products.AsNoTracking().ToArrayAsync();
+            return _mapper.Map < Entities.Product[],Core.Product[]>(result);
+        }
+        public async Task<Product> UpdateProduct(Product product)
+        {
+            var update = _mapper.Map<Core.Product, Entities.Product>(product);
+            var result =  _context.Products.Update(update);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<Entities.Product, Core.Product>(result.Entity);
         }
 
-        public Task<IEnumerable<Product>> GetAllProducts()
+        public async Task<bool> DeleteProduct(int id)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<Product> GetProduct(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Product> UpdateProduct(Product product)
-        {
-            throw new NotImplementedException();
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            _context.Products.Remove(product);
+            return Convert.ToBoolean(await _context.SaveChangesAsync());
         }
     }
 }
