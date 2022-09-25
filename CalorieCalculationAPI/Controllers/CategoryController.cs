14b4e339072e6a384ca75a81283c9cf6799d4710
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-09-26 02:04:58 +0300

    categoryController added

--- CalorieCalculationAPI/Controllers/CategoryController.cs
+++ CalorieCalculationAPI/Controllers/CategoryController.cs
@@ -1,9 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CalorieCalculation.Core;
+using CalorieCalculation.Core.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CalorieCalculationAPI.Controllers
 {
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            var result = await _categoryService.Create(category);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _categoryService.GetById(id);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Category category)
+        {
+            var result = await _categoryService.Update(category);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _categoryService.Delete(id);
+            return Ok(result);
+        }
     }
 }
