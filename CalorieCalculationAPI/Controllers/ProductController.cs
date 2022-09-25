ab46443b0e7f62c9ac4611cf51ed9cdb773625e5
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-09-25 13:30:19 +0300

    a part of the controller has been created

---
+++ CalorieCalculationAPI/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CalorieCalculation.Core;
+using CalorieCalculation.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalorieCalculationAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductService _productService;
+        public ProductController(IMapper mapper, IProductService productService)
+        {
+            _mapper = mapper;
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            var result = await _productService.CreateProduct(product);
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _productService.GetProduct(id);
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _productService.GetAllProducts();
+            return Ok(result);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(Product product)
+        {
+            var result = await _productService.UpdateProduct(product);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            var result = await _productService.DeleteProduct(productId);
+            return Ok(result);
+        }
+    }
+}
--- CalorieCalculationAPI/Program.cs
+++ CalorieCalculationAPI/Program.cs
@@ -1,4 +1,8 @@
+using CalorieCalculation.BusinessLogic;
+using CalorieCalculation.Core.Repositories;
+using CalorieCalculation.Core.Services;
 using CalorieCalculation.DataAccess.Sqlite;
+using CalorieCalculation.DataAccess.Sqlite.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddDbContext<CalorieCalculationContext>(options =>
 {
