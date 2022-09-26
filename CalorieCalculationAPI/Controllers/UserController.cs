8684cb94c8eb2eaaaf125b833a8db5451bf94f32
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-09-26 13:17:00 +0300

    Added: UserController methods

--- CalorieCalculationAPI/Controllers/UserController.cs
+++ CalorieCalculationAPI/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CalorieCalculation.Core;
+using CalorieCalculation.Core.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CalorieCalculationAPI.Controllers
 {
@@ -6,6 +9,41 @@ namespace CalorieCalculationAPI.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
 
+        public UserController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User category)
+        {
+            var result = await _userService.Create(category);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _userService.GetById(id);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(User category)
+        {
+            var result = await _userService.Update(category);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _userService.Delete(id);
+            return Ok(result);
+        }
     }
 }
