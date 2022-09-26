232d3eb163063de1ff921acd04a7a028038e01cd
Issatonk <77848472+Issatonk@users.noreply.github.com>
2022-09-26 13:11:38 +0300

    Added: Empty UserController

---
+++ CalorieCalculationAPI/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalorieCalculationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
